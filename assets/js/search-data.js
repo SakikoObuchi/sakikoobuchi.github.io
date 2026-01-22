3976cc24f0df42def2f7e0a8768fc1fc6125b839
SakikoObuchi <buchi-13526.cjl@fuji.waseda.jp>
2026-01-22 11:25:03 +0000

    Deploying to gh-pages from @ SakikoObuchi/sakikoobuchi.github.io@e78078596f156b74086f1e7bee42776dad76f6cc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,6 +37,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/presentation/";
           },
+        },{id: "nav-presentations",
+          title: "presentations",
+          description: "Materials for presentations you gave. Replace this text with your description.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/presentation/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
