beb7bb1aa5023b138d352cd1e661bbc1fc4673e6
SakikoObuchi <buchi-13526.cjl@fuji.waseda.jp>
2026-01-22 10:40:07 +0000

    Deploying to gh-pages from @ SakikoObuchi/sakikoobuchi.github.io@4e4131eede428fef61bd9e07b6c9137ee9ca416a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-cv",
           title: "cv",
           description: "The PDF version of my Curriculum Vitae is available by clicking the right icon.",
