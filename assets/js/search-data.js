f96c832f2708a8fbf6bcf625885eed4c1d8093c4
SakikoObuchi <buchi-13526.cjl@fuji.waseda.jp>
2026-03-02 14:36:52 +0000

    Deploying to gh-pages from @ SakikoObuchi/sakikoobuchi.github.io@b7be024cdb1456bd6eeca63e7512f554c2261174 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -401,10 +401,12 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-",
-          title: '',
+            },},{id: "news-my-personal-website-is-now-open",
+          title: 'My personal website is now open!',
           description: "",
-          section: "News",},{id: "news-",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_3/";
+            },},{id: "news-",
           title: '',
           description: "",
           section: "News",},{id: "news-",
